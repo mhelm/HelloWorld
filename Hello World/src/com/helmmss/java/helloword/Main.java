cca34c7a9303761b933f979ffefeecaca152aac9
Martin Helm <martin.helm@helmmss.com>
2012-02-04 16:57:30 +0100

    Enhanced greeting.

--- Hello World/src/com/helmmss/java/helloword/Main.java
+++ Hello World/src/com/helmmss/java/helloword/Main.java
@@ -7,7 +7,8 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		System.out.println("Hello world");
+		System.out.println("Hello world.");
+		System.out.println("This is my first github based project.");
 
 	}
 
